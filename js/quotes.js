dec6de89f4cf755ee5fbb3760e8c777e861ab512
Kimsungbin1 <98207899+Kimsungbin1@users.noreply.github.com>
2022-10-29 01:11:24 +0900

    Add files via upload

---
+++ js/quotes.js
@@ -0,0 +1,35 @@
+const quote = document.querySelector("#quote span:first-child");
+const quotes = [
+  {
+    quote:"내 인생은 돈이다 -김성빈-",
+  },
+  {
+    quote:"시간은 곧 돈이다.",
+  },
+  {
+    quote:"지금 당신이 하고있는 것에 최선을 다해라.",
+  },
+  {
+    quote:"고통없인 얻는것도 없다.",
+  },
+  {
+    quote:"마이동풍 (馬耳東風) 너의길을 가라.",
+  },
+  {
+    quote:"인생만사 塞翁之馬(새옹지마).",
+  },
+  {
+    quote:"삶의 원동력은 무엇일까? 첫째도 욕망, 둘째도 욕망, 셋째도 욕망이다. -스탠리 쿠니츠-",
+  },
+  {
+    quote:"내 인생에 포기란 없다.",
+  },
+  {
+    quote:"돈은 가난은 사람을 현명하게도 처절하게도 만든다.",
+  },
+  {
+    quote:"기회는 사라지지 않는다. 니가 놓치는건 딴놈이 잡는다.",
+  },
+];
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+quote.innerText = todaysQuote.quote;
