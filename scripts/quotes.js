ace6a25e25c9d050cee3fe634921c7263a3e8a6b
Kimsungbin1 <ksb19558@naver.com>
2023-02-18 23:21:16 +0900

    1

---
+++ scripts/quotes.js
@@ -0,0 +1,35 @@
+const quote = document.querySelector("#quote span:first-child");
+const quotes = [
+  {
+    quote:"My life is money -sungbin",
+  },
+  {
+    quote:"Time is money",
+  },
+  {
+    quote:"Don't dwell on the past",
+  },
+  {
+    quote:"No pain, no gain",
+  },
+  {
+    quote:"You only live once",
+  },
+  {
+    quote:"I'm still hungry -Guus Hiddink",
+  },
+  {
+    quote:"The unexamined life is not worth living",
+  },
+  {
+    quote:"1. never lose money 2. never forget rule No.1 -Warren Buffett",
+  },
+  {
+    quote:"Successful people always have two things on their lips",
+  },
+  {
+    quote:"Life is all about timing",
+  },
+];
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+quote.innerText = todaysQuote.quote;
