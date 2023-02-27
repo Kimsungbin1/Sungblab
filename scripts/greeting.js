8163ed28864cd50454c9ae384fd35569c0337381
Kimsungbin1 <ksb19558@naver.com>
2023-02-27 14:13:53 +0900

    Update greeting.js

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -6,16 +6,16 @@ const reaminchar = document.getElementById("remain");
 
 const HIDDEN_CLASSNAME = "hidden";
 
-// function onLoginsubmit(event) {
-//   event.preventDefault();
+function onLoginsubmit(event) {
+  event.preventDefault();
 
-//   const username = loginInput.value;
+  // const username = loginInput.value;
 
-//   loginForm.classList.add(HIDDEN_CLASSNAME);
+  // loginForm.classList.add(HIDDEN_CLASSNAME);
 
-//   greeting.innerText = "Hello " + username;
-//   greeting.classList.remove(HIDDEN_CLASSNAME);
-// }
+  // greeting.innerText = "Hello " + username;
+  // greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 loginForm.addEventListener("submit", onLoginsubmit);
 
