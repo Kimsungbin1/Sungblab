d6677ab38cc1dfdbb2e563801a6bcc3bf0a69f93
Kimsungbin1 <ksb19558@naver.com>
2023-02-27 21:12:21 +0900

    1

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,23 +1,5 @@
-// const loginForm = document.querySelector("#login-form");
-// const loginInput = document.querySelector("#login-form input");
-// const greeting = document.querySelector("#greeting");
 const PNIE = document.getElementById("p-name");
 const reaminchar = document.getElementById("remain");
-
-// const HIDDEN_CLASSNAME = "hidden";
-
-// function onLoginsubmit(event) {
-//   event.preventDefault();
-
-//   const username = loginInput.value;
-
-//   loginForm.classList.add(HIDDEN_CLASSNAME);
-
-//   greeting.innerText = "Hello " + username;
-//   greeting.classList.remove(HIDDEN_CLASSNAME);
-// }
-
-// loginForm.addEventListener("submit", onLoginsubmit);
 
 function update(event) {
   const entered = event.target.value;
@@ -27,12 +9,12 @@ function update(event) {
 
   reaminchar.textContent = reaminchars;
 
-  if(reaminchars <= 4) {
-    reaminchar.classList.add('warning');
-    PNIE.classList.add('warning');
+  if (reaminchars <= 4) {
+    reaminchar.classList.add("warning");
+    PNIE.classList.add("warning");
   } else {
-reaminchar.classList.remove('warning');
-PNIE.classList.remove('warning');
+    reaminchar.classList.remove("warning");
+    PNIE.classList.remove("warning");
   }
 }
 
