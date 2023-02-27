f7ac1ea8a432d337afb518657aa753d4783df8b3
Kimsungbin1 <ksb19558@naver.com>
2023-02-27 14:02:10 +0900

    Update greeting.js

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,75 +1,9 @@
-// const PNIE = document.getElementById("p-name");
-// const reaminchar = document.getElementById("remain");
-// const HIDDEN_CLASSNAME = "hidden";
-// function update(event) {
-//   const entered = event.target.value;
-//   const length = entered.length;
 
-//   const reaminchars = 15 - length;
-
-//   reaminchar.textContent = reaminchars;
-
-//   if(reaminchars <= 4) {
-//     reaminchar.classList.add('warning');
-//     PNIE.classList.add('warning');
-//   } else {
-// reaminchar.classList.remove('warning');
-// PNIE.classList.remove('warning');
-//   }
-// }
-
-// PNIE.addEventListener("input", update);
-
-// const greeting = document.querySelector('#greeting');
-// greeting.classList.add(HIDDEN_CLASSNAME);
-
-
-// function onLoginsubmit(event) {
-//   event.preventDefault();
-
-//   const username = loginInput.value;
-
-//   loginForm.classList.add(HIDDEN_CLASSNAME);
-
-//   fetch('/', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-//     body: encode({ 'form-name': 'login-form', 'username': username })
-//   })
-//   .then(() => {
-//     greeting.innerText = "Hello " + username;
-//     greeting.classList.remove(HIDDEN_CLASSNAME);
-//   })
-//   .catch(error => console.error(error));
-// }
-
-// function encode(data) {
-//   return Object.keys(data)
-//     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-//     .join('&');
-// }
-
-// loginForm.addEventListener("submit", onLoginsubmit);
-
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const greeting = document.querySelector('#greeting');
 const PNIE = document.getElementById("p-name");
 const reaminchar = document.getElementById("remain");
 const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginsubmit(event) {
-  event.preventDefault();
-
-  const username = loginInput.value;
-
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-
-  greeting.innerText = "Hello " + username;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
-
-loginForm.addEventListener("submit", onLoginsubmit);
 
 function update(event) {
   const entered = event.target.value;
@@ -79,16 +13,44 @@ function update(event) {
 
   reaminchar.textContent = reaminchars;
 
-  if (reaminchars <= 4) {
-    reaminchar.classList.add("warning");
-    PNIE.classList.add("warning");
+  if(reaminchars <= 4) {
+    reaminchar.classList.add('warning');
+    PNIE.classList.add('warning');
   } else {
-    reaminchar.classList.remove("warning");
-    PNIE.classList.remove("warning");
+reaminchar.classList.remove('warning');
+PNIE.classList.remove('warning');
   }
 }
 
 PNIE.addEventListener("input", update);
+greeting.classList.add(HIDDEN_CLASSNAME);
 
+
+function onLoginsubmit(event) {
+  event.preventDefault();
+
+  const username = loginInput.value;
+
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: encode({ 'form-name': 'login-form', 'username': username })
+  })
+  .then(() => {
+    greeting.innerText = "Hello " + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+  })
+  .catch(error => console.error(error));
+}
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
+
+// loginForm.addEventListener("submit", onLoginsubmit);
 
 
