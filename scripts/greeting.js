4eaf8f1e4e6a5d130f9d53b67abca402e277d823
Kimsungbin1 <ksb19558@naver.com>
2023-02-27 20:50:59 +0900

    Update greeting.js

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,23 +1,23 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+// const loginForm = document.querySelector("#login-form");
+// const loginInput = document.querySelector("#login-form input");
+// const greeting = document.querySelector("#greeting");
 const PNIE = document.getElementById("p-name");
 const reaminchar = document.getElementById("remain");
 
-const HIDDEN_CLASSNAME = "hidden";
+// const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginsubmit(event) {
-  event.preventDefault();
+// function onLoginsubmit(event) {
+//   event.preventDefault();
 
-  const username = loginInput.value;
+//   const username = loginInput.value;
 
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+//   loginForm.classList.add(HIDDEN_CLASSNAME);
 
-  greeting.innerText = "Hello " + username;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
+//   greeting.innerText = "Hello " + username;
+//   greeting.classList.remove(HIDDEN_CLASSNAME);
+// }
 
-loginForm.addEventListener("submit", onLoginsubmit);
+// loginForm.addEventListener("submit", onLoginsubmit);
 
 function update(event) {
   const entered = event.target.value;
