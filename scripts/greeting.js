9f14ce1b18e9b368468a65c213eaf317e1caf147
Kimsungbin1 <ksb19558@naver.com>
2023-02-27 14:07:24 +0900

    Update greeting.js

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,37 +1,10 @@
-// JavaScript
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input[name='username']");
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const HIDDEN_CLASSNAME = "hidden";
 const PNIE = document.getElementById("p-name");
 const reaminchar = document.getElementById("remain");
 
-
-function update(event) {
-  const entered = event.target.value;
-  const length = entered.length;
-  const remainChars = 15 - length;
-
-  remainChar.textContent = remainChars;
-
-  if(remainChars <= 4) {
-    remainChar.classList.add('warning');
-    PNIE.classList.add('warning');
-  } else {
-    remainChar.classList.remove('warning');
-    PNIE.classList.remove('warning');
-  }
-}
-
-PNIE.addEventListener("input", update);
-greeting.classList.add(HIDDEN_CLASSNAME);
-
-function onLoginSubmit(event) 
-  event.preventDefault();
-
-  const username = loginInput.value;
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginsubmit(event) {
   event.preventDefault();
@@ -40,24 +13,27 @@ function onLoginsubmit(event) {
 
   loginForm.classList.add(HIDDEN_CLASSNAME);
 
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: encode({ 'form-name': 'login-form', 'username': username })
-  })
-  .then(() => {
-    greeting.innerText = "Hello " + username;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-  })
-  .catch(error => console.error(error));
-}
-
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginsubmit);
 
+function update(event) {
+  const entered = event.target.value;
+  const length = entered.length;
 
+  const reaminchars = 15 - length;
+
+  reaminchar.textContent = reaminchars;
+
+  if(reaminchars <= 4) {
+    reaminchar.classList.add('warning');
+    PNIE.classList.add('warning');
+  } else {
+reaminchar.classList.remove('warning');
+PNIE.classList.remove('warning');
+  }
+}
+
+PNIE.addEventListener("input", update);
