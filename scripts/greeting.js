bf5eed169be3af4cc9c6cee9ddc93f8c19a73d4e
Kimsungbin1 <ksb19558@naver.com>
2023-02-27 13:47:31 +0900

    1

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,39 +1,62 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
-const PNIE = document.getElementById("p-name");
-const reaminchar = document.getElementById("remain");
+// const loginForm = document.querySelector("#login-form");
+// const loginInput = document.querySelector("#login-form input");
+// const greeting = document.querySelector("#greeting");
+// const PNIE = document.getElementById("p-name");
+// const reaminchar = document.getElementById("remain");
 
-const HIDDEN_CLASSNAME = "hidden";
+// const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginsubmit(event) {
+// function onLoginsubmit(event) {
+//   event.preventDefault();
+
+//   const username = loginInput.value;
+
+//   loginForm.classList.add(HIDDEN_CLASSNAME);
+
+//   greeting.innerText = "Hello " + username;
+//   greeting.classList.remove(HIDDEN_CLASSNAME);
+// }
+
+// loginForm.addEventListener("submit", onLoginsubmit);
+
+// function update(event) {
+//   const entered = event.target.value;
+//   const length = entered.length;
+
+//   const reaminchars = 15 - length;
+
+//   reaminchar.textContent = reaminchars;
+
+//   if(reaminchars <= 4) {
+//     reaminchar.classList.add('warning');
+//     PNIE.classList.add('warning');
+//   } else {
+// reaminchar.classList.remove('warning');
+// PNIE.classList.remove('warning');
+//   }
+// }
+
+// PNIE.addEventListener("input", update);
+
+const greeting = document.querySelector('#greeting');
+greeting.classList.add(HIDDEN_CLASSNAME);
+
+
+const handleFormSubmit = async (event) => {
   event.preventDefault();
-
-  const username = loginInput.value;
-
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-
-  greeting.innerText = "Hello " + username;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
-
-loginForm.addEventListener("submit", onLoginsubmit);
-
-function update(event) {
-  const entered = event.target.value;
-  const length = entered.length;
-
-  const reaminchars = 15 - length;
-
-  reaminchar.textContent = reaminchars;
-
-  if(reaminchars <= 4) {
-    reaminchar.classList.add('warning');
-    PNIE.classList.add('warning');
-  } else {
-reaminchar.classList.remove('warning');
-PNIE.classList.remove('warning');
+  const formData = new FormData(event.target);
+  const response = await fetch(event.target.action, {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+  if (response.ok) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = formData.get('p-name');
+    greeting.innerText = `Hello ${username}`;
   }
-}
+};
 
-PNIE.addEventListener("input", update);
