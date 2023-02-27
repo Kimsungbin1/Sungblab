fd0f843d703f134520dbbf0d1be113ace757483e
Kimsungbin1 <ksb19558@naver.com>
2023-02-27 13:55:04 +0900

    1

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,5 +1,22 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 const PNIE = document.getElementById("p-name");
 const reaminchar = document.getElementById("remain");
+
+function onLoginsubmit(event) {
+  event.preventDefault();
+
+  const username = loginInput.value;
+
+  loginForm.classList.add("hidden");
+
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove("hidden");
+}
+
+loginForm.addEventListener("submit", onLoginsubmit);
+
 function update(event) {
   const entered = event.target.value;
   const length = entered.length;
@@ -12,60 +29,12 @@ function update(event) {
     reaminchar.classList.add('warning');
     PNIE.classList.add('warning');
   } else {
-reaminchar.classList.remove('warning');
-PNIE.classList.remove('warning');
+    reaminchar.classList.remove('warning');
+    PNIE.classList.remove('warning');
   }
 }
 
 PNIE.addEventListener("input", update);
 
-const greeting = document.querySelector('#greeting');
-greeting.classList.add(HIDDEN_CLASSNAME);
-
-
-// const handleFormSubmit = async (event) => {
-//   event.preventDefault();
-//   const formData = new FormData(event.target);
-//   const response = await fetch(event.target.action, {
-//     method: 'POST',
-//     body: formData,
-//     headers: {
-//       'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//   });
-//   if (response.ok) {
-//     greeting.classList.remove(HIDDEN_CLASSNAME);
-//     loginForm.classList.add(HIDDEN_CLASSNAME);
-//     const username = formData.get('p-name');
-//     greeting.innerText = `Hello ${username}`;
-//   }
-// };
-
-function onLoginsubmit(event) {
-  event.preventDefault();
-
-  const username = loginInput.value;
-
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: encode({ 'form-name': 'login-form', 'username': username })
-  })
-  .then(() => {
-    greeting.innerText = "Hello " + username;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-  })
-  .catch(error => console.error(error));
-}
-
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
-}
-
-loginForm.addEventListener("submit", onLoginsubmit);
 
 
