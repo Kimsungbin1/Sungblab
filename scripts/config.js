9492ec575ed5ee1966b1001556e79d99c5d87c88
Kimsungbin1 <98207899+Kimsungbin1@users.noreply.github.com>
2024-05-27 23:24:26 +0900

    2

--- scripts/config.js
+++ scripts/config.js
@@ -1,29 +1,28 @@
 function openplayer(event) {
-    editedplayer = +event.target.dataset.playerid;
-    playerover.style.display = "block";
-    backdrope.style.display = "block";
+  editedplayer = +event.target.dataset.playerid;
+  playerover.style.display = "block";
+  backdrope.style.display = "block";
+}
+
+function closedonfig() {
+  playerover.style.display = "none";
+  backdrope.style.display = "none";
+  forme.firstElementChild.lastElementChild.value = "";
+}
+
+function saveplayerconfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredname = formData.get("username");
+
+  if (document.getElementById("playername").value.trim() == "") {
+    alert("Please enter correctly");
   }
-  
-  function closedonfig() {
-    playerover.style.display = "none";
-    backdrope.style.display = "none";
-    forme.firstElementChild.lastElementChild.value = '';
-  }
-  
-  function saveplayerconfig(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const enteredname = formData.get("username");
-  
-    if (document.getElementById("playername").value.trim() == "") {
-      alert("Please enter correctly");
-    }
-  
-    const updated = document.getElementById("player-" + editedplayer + "-data");
-    updated.children[1].textContent = enteredname;
-  
-    players[editedplayer - 1].name = enteredname;
-  
-    closedonfig();
-  }
-  
+
+  const updated = document.getElementById("player-" + editedplayer + "-data");
+  updated.children[1].textContent = enteredname;
+
+  players[editedplayer - 1].name = enteredname;
+
+  closedonfig();
+}
